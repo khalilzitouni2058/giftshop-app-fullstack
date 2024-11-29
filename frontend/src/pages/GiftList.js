850da7d24581f15fa5dc30462e3555b1751eed3f
khalilzitouni <114951550+khalilzitouni2058@users.noreply.github.com>
2024-11-29 21:17:36 +0100

    updated the navbar of giftList

--- frontend/src/pages/GiftList.js
+++ frontend/src/pages/GiftList.js
@@ -4,17 +4,29 @@ import ListOfIcons from '../components/ListOfIcons'
 import GiftCard from '../components/GiftCard'
 import { useState } from 'react'
 import { Navbar } from 'react-bootstrap'
-
+import { FaSearch } from "react-icons/fa";
+import { MdLogin } from "react-icons/md";
+import { FaShoppingBag } from "react-icons/fa";
+import Button from 'react-bootstrap/Button';
 function GiftList({products}) {
 
   
   return (
   <>
 
-  <NavBar />
+<nav>
+        <a href='/' className='brand'>Giftshop</a>
+       
+        <ul className='nav_menu'>
+            <li className='nav_item'><Button  variant="outline-light"><MdLogin /></Button></li>
+            <li className='nav_item'><Button variant="outline-light"><FaSearch /></Button></li>
+            <li className='nav_item'><Button variant="outline-light"><FaShoppingBag /></Button></li>
+        </ul>
+        
+    </nav>
 
    
-    <div style={{}}>
+    <div style={{marginTop:"-20px"}}>
     <ListOfIcons />
 <div style={{ display: "flex", gap: "50px", marginTop:"100px",justifyContent:"center", flexWrap:"wrap" }}>
 {products.map((product) =>( <GiftCard product={product} subImage1={product.subImage1}
