f3547975b44a97ffbe72342c12b8942f45ad194d
khalilzitouni <114951550+khalilzitouni2058@users.noreply.github.com>
2024-11-30 12:25:40 +0100

    updated the routing in giftlist and sign in

---
+++ frontend/src/components/SignIn.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+import floflo from "../assets/floflo.jpg"
+import NavBar from '../components/NavBar'
+function SignIn() {
+  return (
+    <>
+    <NavBar />
+    <div style={{display:"flex",alignItems:"center"}}>
+        {/** left  slide */}
+        <div  className="hero min-h-screen" style={{width:"1000px",height:"730px",backgroundImage: `url(${floflo})`}}>
+        <div className="hero-overlay bg-opacity-60">
+        <h1 className='mb-5 text-5xl font-bold  text-zinc-50 ' style={{font:"Anton",textTransform:"uppercase",marginTop:"230px",marginLeft:"200px",fontSize:"50px"}}> Join Us</h1>
+            <h1 className='mb-5 text-5xl font-bold  text-zinc-50 ' style={{font:"Anton",textTransform:"uppercase",marginLeft:"200px",fontSize:"50px"}}> unwrap  the joy of giving!</h1>
+
+        </div>
+           
+        </div>
+        {/** right slide */}
+        <div
+      style={{
+        backgroundColor: "rgb(254 215 170)", // Light gift shop-themed color
+        width: "1000px",
+        height: "730px",
+        
+        padding: "20px",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        
+      }}
+    >
+      <h1 style={{ fontFamily: "Anton",textTransform:"uppercase", fontSize:"30px",marginBottom: "20px", color: "#8b4513" }}>
+        Welcome TO Hdeya
+      </h1>
+
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "80%",
+          maxWidth: "400px",
+        }}
+      >
+        <label style={{ fontWeight: "bold", marginBottom: "8px", color: "#8b4513" }}>
+          UserName
+        </label>
+        <input
+          type="UserName"
+          placeholder="Enter your Username"
+          style={{
+            padding: "10px",
+            marginBottom: "20px",
+            borderRadius: "18px",
+            
+            fontSize: "16px",
+            backgroundColor:"rgb(228 228 231)"
+          }}
+        />
+        <label style={{ fontWeight: "bold", marginBottom: "8px", color: "#8b4513" }}>
+          Email Address
+        </label>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          style={{
+            padding: "10px",
+            marginBottom: "20px",
+            borderRadius: "18px",
+            border: "1px solid #ccc",
+            fontSize: "16px",
+            backgroundColor:"rgb(228 228 231)"
+          }}
+        />
+        
+
+        <label style={{ fontWeight: "bold", marginBottom: "8px", color: "#8b4513" }}>
+          Password
+        </label>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          style={{
+            padding: "10px",
+            marginBottom: "20px",
+            borderRadius: "18px",
+            border: "1px solid #ccc",
+            fontSize: "16px",
+            backgroundColor:"rgb(228 228 231)"
+          }}
+        />
+        <label style={{ fontWeight: "bold", marginBottom: "8px", color: "#8b4513" }}>
+          Date of birth
+        </label>
+        <input
+          type="date"
+          placeholder="Enter your Date of birth"
+          style={{
+            padding: "10px",
+            marginBottom: "20px",
+            borderRadius: "18px",
+            border: "1px solid #ccc",
+            fontSize: "16px",
+            backgroundColor:"rgb(228 228 231)"
+          }}
+        />
+        <label style={{ fontWeight: "bold", marginBottom: "8px", color: "#8b4513" }}>
+          Phone Number
+        </label>
+        <input
+          type="text"
+          placeholder="Enter your Phone Number"
+          style={{
+            padding: "10px",
+            marginBottom: "20px",
+            borderRadius: "18px",
+            border: "1px solid #ccc",
+            fontSize: "16px",
+            backgroundColor:"rgb(228 228 231)"
+          }}
+        />
+
+        <button
+          type="submit"
+          style={{
+            padding: "10px",
+            backgroundColor: "#ff7f50", // Coral color for a friendly vibe
+            color: "rgb(228 228 231)",
+            fontSize: "18px",
+            fontWeight: "bold",
+            border: "none",
+            borderRadius: "8px",
+            cursor: "pointer",
+            marginTop:"30px",
+            transition: "background-color 0.3s",
+          }}
+          
+        >
+          Sign Up
+        </button>
+
+        
+      </form>
+    </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default SignIn
