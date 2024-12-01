59a71a3d8b2ba2de96e4fe9a4ee1645453c18eff
Alaaeddine bousselmi <alaaeddine.bousselmi@medtech.tn>
2024-12-01 14:57:39 +0100

    removeAge

--- backend/model/User.js
+++ backend/model/User.js
@@ -11,10 +11,6 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    age: {
-        type: String,
-        required: true,
-    },
     password: {
         type: String,
         required: true
