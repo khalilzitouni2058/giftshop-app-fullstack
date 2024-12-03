1546be6771c0559a96c7a5e935f848082832b681
Alaaeddine bousselmi <alaaeddine.bousselmi@medtech.tn>
2024-12-03 16:37:08 +0100

    added getProductById

--- backend/Controllers/productsController.js
+++ backend/Controllers/productsController.js
@@ -19,6 +19,22 @@ const getProductsByCategory = async (req, res) => {
     }
 };
 
+const getProductById = async (req, res) => {
+    const id = req.params.id; 
+    try {
+        const product = await Product.findById(id); 
+        if (product) {
+            res.status(200).json({ product }); 
+        } else {
+            res.status(404).json({ msg: "Product not found" }); 
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "Error retrieving product" }); 
+    }
+};
+
+
 
 // const postOneProduct = async (req, res) => {
 //     const product = req.body;
@@ -69,4 +85,5 @@ module.exports = {
     getProductsByCategory,
     postProduct,
     deleteProduct,
+    getProductById,
 };
--- backend/Routes/productsRoute.js
+++ backend/Routes/productsRoute.js
@@ -4,12 +4,16 @@ const {
     getProductsByCategory,
     postProduct,
     deleteProduct,
+    getProductById,
     
 } = require("../Controllers/productsController");
 const isAuth = require("../middleware/isAuth")
 const isAutho=require('../middleware/isAutho')
 
+
 productsRoute.get("/products/:category", getProductsByCategory);
+
+productsRoute.get("/products/:category/:id", getProductById);
 productsRoute.post("/products", postProduct);
 productsRoute.delete("/products/:title",isAuth,isAutho(['admin']),deleteProduct)
 
